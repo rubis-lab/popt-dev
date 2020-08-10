287b10882bd6370193f7a68fe96b41dd8ccf37e4
Jaeseung Park <jadenpark98@gmail.com>
2020-08-10 17:49:19 +0900

    run-time popt specification

---
+++ OpenMP/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+int main(int argc, char *arvg[]){
+    int n = 4;
+    for(int i = 1; i < 4; i++){
+        printf("Running %d threads...\n", n);
+        omp_set_dynamic(0);
+        omp_set_num_threads(n);
+        #pragma omp parallel
+        {
+            printf("Hello, World! %d\n", omp_get_thread_num());
+        }
+        n += i;
+    }
+    return 0;
+}
