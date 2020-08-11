b96a4b77315975a56f61fbe985782c808b04b0a7
Youngeun Cho <upoque@gmail.com>
2020-08-11 12:03:49 +0900

    qt gui skeleton added

---
+++ gui/gui.py
@@ -0,0 +1,14 @@
+import sys
+from rtp_funcs import RTPFuncs
+from PyQt5.QtWidgets import QApplication
+
+
+def start_gui():
+    app = QApplication(sys.argv)
+    rtp_ui = RTPFuncs()
+    rtp_ui.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    start_gui()
---
+++ gui/rtp_funcs.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import uic
+rtp_ui = uic.loadUiType('rtp.ui')[0]
+
+
+class RTPFuncs(QMainWindow, rtp_ui):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.setupUi(self)
+
+        print('rtp_ui -- init')
+        self.btn_test.clicked.connect(self.test_func)
+        return
+
+    def test_func(self):
+        print('test_btn clicked.')
+        return
