3b6ce1df39c86405b2d4e320641bfbac66e9a4db
Jaeseung Park <jadenpark98@gmail.com>
2020-08-12 16:59:17 +0900

    created task and task_set

--- rts/src/task.cpp
+++ rts/src/task.cpp
@@ -1,9 +1,49 @@
 #include "task.hpp"
+using namespace std;
 namespace rts {
+    Task::Task(){
+        task_print();
+    }
 
-int task_print(int n) {
-    std::cout << "task_print: " << n << std::endl;
-    return 0;
-}
+    void Task::task_print(){
+        if(task_id == -1){
+            cout << "task does not exist; create task_set object first" 
+                << endl;
+        } else {
+            cout << task_id << "\t" << exec_time << "\t" << 
+            deadline << "\t" << period << "\t" << popt<< endl;
+        }
+    }
 
+    void Task::set_task_id(int id){
+        task_id = id;
+    }
+    void Task::set_popt(int pt){
+        popt = pt;
+    }
+    void Task::set_exec_time(int ext){
+        exec_time = ext;
+    }
+    void Task::set_deadline(int dl){
+        deadline = dl;
+    }
+    void Task::set_period(int prd){
+        period = prd;
+    }
+
+    int Task::get_task_id(){
+        return task_id;
+    }
+    int Task::get_popt(){
+        return popt;
+    }
+    int Task::get_exec_time(){
+        return exec_time;
+    }    
+    int Task::get_deadline(){
+        return deadline;
+    }
+    int Task::get_period(){
+        return period;
+    }
 }
--- samples/dev/src/main.cpp
+++ samples/dev/src/main.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    // if(argc < 2) {
-    //     cout << "usage: ./dev " << endl;
-    //     return -1;
-    // }
-    cout << "(dev)" << endl;
-    rts::task_print(1);
+    rts::Task temp;
+    // temp.set_task_id(1);
+    // temp.set_popt(5);
+    // temp.set_exec_time(10);
+    // temp.set_deadline(10);
+    // temp.set_period(50);
+    // temp.task_print();
     return 0;
 }
