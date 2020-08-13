29a683a8cf1ae11891f2dae6b24f5821907645f6
Akyeast <akyeast@gmail.com>
2020-08-13 22:46:36 +0900

    CMake change

---
+++ rts/include/gen.hpp
@@ -0,0 +1,22 @@
+// #ifndef __GEN_H__
+// #define __GEN_H__
+
+// #include "task_set.hpp"
+// #include <cstdlib>
+// #include <random>
+
+// namespace rts {
+
+// class Gen {
+// public:
+
+//     Gen();
+//     ~Gen();
+//     std::string to_str();
+//     Task next_task();
+//     TaskSet next_taskset();
+//     int get_random_number(int min, int max);
+// };
+
+// }  // namespace rts
+// #endif  // __GEN_H__
---
+++ rts/src/gen.cpp
@@ -0,0 +1,13 @@
+// #include "gen.hpp"
+
+
+// GEN::get_random_number(int min, int max) {
+//     std::random_device rn;
+//     std::mt19937_64 rnd(rn());
+//     std::uniform_int_distribution<int> range(min, max);
+//     return range(rnd);   
+// }
+
+// GEN::GEN() {
+
+// }
