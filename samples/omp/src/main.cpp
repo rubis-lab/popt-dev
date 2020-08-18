13fd6a56b2006d882d551532d7b0ac2eb109ea0e
Youngeun Cho <upoque@gmail.com>
2020-08-18 21:17:17 +0900

    #41r89vv[complete] moved eg1 into samples

--- samples/omp/src/main.cpp
+++ samples/omp/src/main.cpp
@@ -4,16 +4,12 @@ using namespace std;
 
 int main(int argc, char **argv) {
     if(argc < 2) {
-        cout << "usage: ./EG1 sample.json" << endl;
+        cout << "usage: ./omp exp1.json ts1.json" << endl;
         return -1;
     }
     cout << "main thread id: " << gettid() << endl;
-    //temp vector
-    vector<int> temp;
-    temp.push_back(1);
-    temp.push_back(4);
 
-    SchedExp se(argv[1], temp);
+    SchedExp se(argv[1], argv[2]);
     se.run();
     cout << "main dies: " << gettid() << endl;
     return 0;
