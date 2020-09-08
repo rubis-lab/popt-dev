363095b1330f0f027aa76460e1872745291c85fc
Akyeast <akyeast@gmail.com>
2020-09-08 17:19:51 +0900

    bcl_slack dev

--- rts/src/sched/bcl.cpp
+++ rts/src/sched/bcl.cpp
@@ -24,6 +24,7 @@ double BCL::calc_interference(Task _base_task, Task _inter_task) {
     return i_sum;
 }
 
+
 double BCL::sum_interference(TaskSet _ts, Task _base_task) {
     double sum_j = 0.0;
     for(Task inter_task : _ts.tasks){
@@ -43,4 +44,47 @@ bool BCL::is_schedulable(TaskSet _ts) {
     }
     return true;
 }
+
+ 
+//slack
+double BCL::calc_interference_slack(Task _base_task, Task _inter_task) {
+    TSUtil tsutil;
+    double i_sum = tsutil.workload_in_interval_edf_slack(_inter_task, _base_task.deadline);
+    i_sum = std::max(0.0, std::min(i_sum, 
+        _base_task.deadline - _base_task.exec_time + 1.0));
+    return i_sum;
+}
+
+double BCL::sum_interference_slack(TaskSet _ts, Task _base_task) {
+    double sum_j = 0.0;
+    for(Task inter_task : _ts.tasks){
+        if (inter_task != _base_task){
+            sum_j += calc_interference_slack(_base_task, inter_task);
+        }
+    }
+    return floor(sum_j / num_core);
+}
+
+bool BCL::is_schedulable_slack(TaskSet _ts) {
+    bool slack_updated = true;
+    bool potentially_not_schedulable = true;
+    double slack;
+    while(slack_updated) {
+        slack_updated = false;
+        for(Task base_task: _ts.tasks) {
+            double interference = sum_interference_slack(_ts, base_task);
+            slack = (base_task.deadline - base_task.exec_time) + 0.1 - interference;
+            if(slack > base_task.slack) {
+                base_task.slack = slack;
+                slack_updated = true;
+            } else if(slack < 0) {
+                potentially_not_schedulable = true;
+            }
+        }
+        if(!potentially_not_schedulable)
+            return true;
+    }
+    return false;
+}
+
 }  // namespace rts
