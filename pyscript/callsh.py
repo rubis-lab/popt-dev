d4359c359c33d4e6ad9b76b8bec0b8071ad51cc6
akyeast <akyeast@gmail.com>
2021-10-26 23:23:02 +0900

    sh check

--- pyscript/callsh.py
+++ pyscript/callsh.py
@@ -6,6 +6,8 @@ import math
 
 print("hi")
 
-os.system("sh test.sh 1 2")
+for _ in range(1, 11):
+    os.system("sh test.sh 1 2")
+    print('{}\'s shell script finished'.format(_))
 
 exit()
--- pyscript/logparser.py
+++ pyscript/logparser.py
@@ -5,14 +5,12 @@ import math
 
 log_dir = '/home/rtss/rt-app/rubis/log'
 
-logs = []
-
 def f2s(fullname):
-    if len(f.split('.')) > 2:
+    if len(fullname.split('.')) > 2:
         return False
-    if f.split('.')[-1] != 'log':
+    if fullname.split('.')[-1] != 'log':
         return False
-    filename = f.split('.log')[0]
+    filename = fullname.split('.log')[0]
     if filename.split('.')[-1] == 'swp':
         return False
     if filename.split('-')[0] != 'rubis':
@@ -22,63 +20,73 @@ def f2s(fullname):
 def s2f(shortname):
     return 'rubis-'+shortname+'.log'
 
-for f in os.listdir(log_dir):
-    shortname = f2s(f)
-    if shortname is not False:
-        logs.append(shortname.split('rubis-')[-1])
+def parselog():
+    logs = []
 
+    for f in os.listdir(log_dir):
+        shortname = f2s(f)
+        if shortname is not False:
+            logs.append(shortname.split('rubis-')[-1])
 
-logdatas = {}
+    logdatas = {}
 
+    for log in logs:
+        with open(log_dir+'/'+s2f(log), 'r') as lines:
+            newdict = {}
+            header = []
+            data = []     
+            # exception not handled. assume log is fine
+            for line in lines:
+                linedata = []
 
-for log in logs:
-    with open(log_dir+'/'+s2f(log), 'r') as lines:
-        newdict = {}
-        header = []
-        data = []     
-        # exception not handled. assume log is fine
-        for line in lines:
-            linedata = []
+                arr = ' '.join(line.split()).split(' ')
+                # print(logstr)
 
-            arr = ' '.join(line.split()).split(' ')
-            # print(logstr)
+                if arr[0] == '#idx':
+                    arr[0] = arr[0].split('#')[-1]
+                    for _ in arr:
+                        header.append(_)
+                    # print(newdict)
+                else:
+                    for _ in arr:
+                        linedata.append(_)
+                    data.append(linedata)
+            
+            newdict['header'] = header
+            newdict['data'] = data
 
-            if arr[0] == '#idx':
-                arr[0] = arr[0].split('#')[-1]
-                for _ in arr:
-                    header.append(_)
-                # print(newdict)
-            else:
-                for _ in arr:
-                    linedata.append(_)
-                data.append(linedata)
+            logdatas[log] = newdict
         
-        newdict['header'] = header
-        newdict['data'] = data
+            return logdatas
 
-        logdatas[log] = newdict
-        
-        print(logdatas)
+# True or False
+def schedulable(logdatas):
+    return True
 
-{
-    'thr0-1': {
-        'header': ['idx', 'perf', ...],
-        'data': [
-            ['1', '6000', ...],
-            ['1', '6000', ...],
-            ['1', '6000', ...],
-            ['1', '6000', ...],
-            ['1', '6000', ...],
-        ]
-    },
-    'thr1-0': {
-        'header': ['idx', 'perf', ...],
-        'data': [
-            ['1', '6000', ...],
-            ['1', '6000', ...],
-            ['1', '6000', ...],
-            ['1', '6000', ...],
-            ['1', '6000', ...],
-        ]
-    }
-}
+parselog()
+
+
+
+
+# {
+#     'thr0-1': {
+#         'header': ['idx', 'perf', ...],
+#         'data': [
+#             ['1', '6000', ...],
+#             ['1', '6000', ...],
+#             ['1', '6000', ...],
+#             ['1', '6000', ...],
+#             ['1', '6000', ...],
+#         ]
+#     },
+#     'thr1-0': {
+#         'header': ['idx', 'perf', ...],
+#         'data': [
+#             ['1', '6000', ...],
+#             ['1', '6000', ...],
+#             ['1', '6000', ...],
+#             ['1', '6000', ...],
+#             ['1', '6000', ...],
+#         ]
+#     }
+# }
