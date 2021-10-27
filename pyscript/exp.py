7865b16e855a312b6e8f274a32000ed10ea13b80
akyeast <akyeast@gmail.com>
2021-10-27 19:44:37 +0900

    exp.py run 1

--- pyscript/exp.py
+++ pyscript/exp.py
@@ -4,11 +4,34 @@ from time import time
 import math
 import logparser
 import schedulable
+import json
 
-jsondir = '/home/rtss/rt-app/rubis'
+jsondir = '/home/rtss/json'
+
+
+def create_logdir(jsonfile):
+    with open(jsonfile, "r") as f:
+        j = json.loads(f.read())
+
+    logdir = j["global"]["logdir"]
+    os.mkdir(logdir)
+    return
 
 if __name__=="__main__":
-    # os.system("rt-app "+jsondir+"/rubis_test.json")
+    
+    jsonfiles = os.listdir(jsondir)
+
+
+    for jsonfile in jsonfiles:
+        if jsonfile.split(".")[-1] != "json":
+            continue
+        jsonfile_path = os.path.join(jsondir, jsonfile)
+        print(f"running {jsonfile_path}...")
+        create_logdir(jsonfile_path)
+        os.system(f"rt-app {jsonfile_path}")
+        
+        exit()
+
     sch = schedulable.missoccur(logparser.parse_currentlogs())
     print(sch)
     is_schedulable = schedulable.schedulable(sch)
